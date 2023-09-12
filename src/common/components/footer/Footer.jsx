967352a62016a5b4317cb98bf4dd635b0af498c9
Tumelo Mahlangu <tumie201511@gmail.com>
2023-09-12 03:32:32 +0200

    updated components

--- src/common/components/footer/Footer.jsx
+++ src/common/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './footer.scss';
+import SocialLinks from './components/SocialLinks';
 
 function Footer() {
   return (
@@ -15,6 +16,14 @@ function Footer() {
                     <p>+27 21 431 0001</p>
                     <a href='/'>enquirie@website.co.za</a>
                 </div>
+            </div>
+
+            <div className='footer-links pd-vertically-2'>
+                <SocialLinks socialLink1={'Terms of service'} socialLink2={'Privacy policy'} socialLink3={'Impressum'}/>
+                <SocialLinks socialLink1={'Facebook'} socialLink2={'Instagram'} socialLink3={'Twitter'}/>
+                <SocialLinks socialLink1={'Github'} socialLink2={'Linkedin'} socialLink3={'Teams'}/>
+                <SocialLinks socialLink1={'YouTube'} socialLink2={'Behance'} socialLink3={'Dribble'}/>
+                <SocialLinks socialLink1={'Explore open jobs'} socialLink2={''} socialLink3={'©2000—2023 Company Name'}/>
             </div>
         </div>
     </div>
--- src/common/components/footer/components/SocialLinks.jsx
+++ src/common/components/footer/components/SocialLinks.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './socialLinks.scss'
 
-function SocialLinks() {
+function SocialLinks({socialLink1, socialLink2, socialLink3}) {
   return (
-    <div>SocialLinks</div>
+    <div className='social-links'>
+        <a href='#link'>
+            <p>{socialLink1}</p>    
+        </a>
+        <a href='#link'>
+            <p>{socialLink2}</p>
+        </a>
+        <a href='#link'>
+            <p>{socialLink3}</p>
+        </a>
+    </div>
   )
 }
 
